78c44469dd71a02a9717bae29128c4b7f48df9dc
smallsun <smallsun107@gmail.com>
2025-04-11 20:29:41 +0800

    fix errors.

--- Formula/behinder.rb
+++ Formula/behinder.rb
@@ -6,12 +6,6 @@ class Behinder < Formula
     sha256 '642d4a3930d08a3b54828c661eab62021505938bc5c21b2af04d10218b2de5d7'
 
     def install
-        ENV["LANG"] = "en_US.UTF-8"  # 解决 unzip 非 UTF-8 文件名报错问题
-        ENV["LC_ALL"] = "en_US.UTF-8"
-        # 先手动解压，并指定文件名编码（GBK 是中国大陆打包 zip 常用编码）
-        mkdir_p libexec
-        system "unzip", "-O", "GBK", cached_download, "-d", libexec
-
         libexec.install Dir['*']
         (bin/"behinder").write <<~EOS
             #!/bin/bash
