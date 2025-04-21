4861191cef64d5e53c6de82d9d2b78b900933484
smallsun <smallsun107@gmail.com>
2025-04-21 12:43:38 +0800

    fix errors.

--- Formula/godzillaekp.rb
+++ Formula/godzillaekp.rb
@@ -5,8 +5,18 @@ class Godzillaekp < Formula
     version '1.1'
     sha256 '3b4652226c5fee963ce29edde5a82add6bd2b074bec4a9c9f3b40c94bd0e35e1'
 
+    # def install
+    #     libexec.install Dir['*']
+    #     bin.write_jar_script libexec/'Godzilla_ekp1.1.jar', 'godzilla_ekp'
+    # end
     def install
-        libexec.install Dir['*']
-        bin.write_jar_script libexec/'Godzilla_ekp1.1.jar', 'godzilla_ekp'
+        libexec.install "Godzilla_ekp1.1.jar"
+        (bin/"godzilla_ekp").write <<~EOS
+            #!/bin/bash
+            mkdir -p "$HOME/.config/godzilla_ekp"
+            cd "$HOME/.config/godzilla_ekp"
+            exec java -jar "#{libexec}/Godzilla_ekp1.1.jar" "$@"
+        EOS
+        chmod 0755, bin/"godzilla_ekp"
     end
 end
