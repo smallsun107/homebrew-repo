79bc0d134dd541d4da8db7a3d194e4e450bdcc1b
smallsun <smallsun107@gmail.com>
2025-04-21 12:33:38 +0800

    fix errors.

--- Formula/godzilla.rb
+++ Formula/godzilla.rb
@@ -5,8 +5,18 @@ class Godzilla < Formula
     version '4.0.1'
     sha256 '62b7eff31d339fd04e6d39aba47b5f37b1b6feb27f85c3c71e4d2d600e8142c6'
 
+    # def install
+    #     libexec.install Dir['*']
+    #     bin.write_jar_script libexec/'godzilla.jar', 'godzilla'
+    # end
     def install
-        libexec.install Dir['*']
-        bin.write_jar_script libexec/'godzilla.jar', 'godzilla'
+        libexec.install "godzilla.jar"
+        (bin/"godzilla").write <<~EOS
+            #!/bin/bash
+            mkdir -p "$HOME/.config/godzilla"
+            cd "$HOME/.config/godzilla"
+            exec java -jar "#{libexec}/godzilla.jar" "$@"
+        EOS
+        chmod 0755, bin/"godzilla"
     end
 end
